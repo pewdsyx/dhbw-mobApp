1aa39a537812a58eabc9aece5f94a81865ce3f39
Yves Staudenmaier <yves.staudenmaier@gmail.com>
2019-11-26 17:10:19 +0100

    simple calculator

--- HelloWorld/app/src/main/java/com/dhbw/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/dhbw/helloworld/MainActivity.java
@@ -21,8 +21,35 @@ public class MainActivity extends AppCompatActivity {
     // click listner for our button:
     public void onButtonClick(View aView) {
         System.out.println("onButtonClick");
-        TextView textView = findViewById(R.id.textToChange);
-        EditText editView = findViewById(R.id.editText);
-        textView.setText(editView.getText());
+        TextView wert1 = findViewById(R.id.wert1);
+        TextView wert2 = findViewById(R.id.wert2);
+        TextView operant = findViewById(R.id.wert3);
+
+        TextView ergebnis = findViewById(R.id.ergebnis);
+        double i = 0.0;
+        System.out.println(operant.getText().toString());
+
+        switch (operant.getText().toString()) {
+            case "+":
+                i = Double.parseDouble(wert1.getText().toString()) + Double.parseDouble(wert2.getText().toString());
+                ergebnis.setText(Double.toString(i));
+                break;
+            case "-":
+                i = Double.parseDouble(wert1.getText().toString()) - Double.parseDouble(wert2.getText().toString());
+                ergebnis.setText(Double.toString(i));
+                System.out.println(i);
+                break;
+            case "*":
+                i = Double.parseDouble(wert1.getText().toString()) * Double.parseDouble(wert2.getText().toString());
+                ergebnis.setText(Double.toString(i));
+                break;
+            case "/":
+                i = Double.parseDouble(wert1.getText().toString()) / Double.parseDouble(wert2.getText().toString());
+                ergebnis.setText(Double.toString(i));
+                break;
+            default:
+                System.out.println("default");
+                break;
+        }
     }
 }
