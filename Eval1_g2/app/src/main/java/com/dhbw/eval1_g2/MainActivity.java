f3bd5e2a71257e484fa44d6fa30e2156c9143b83
Yves Staudenmaier <yves.staudenmaier@gmail.com>
2019-11-28 14:28:48 +0100

    initial commit

---
+++ Eval1_g2/app/src/main/java/com/dhbw/eval1_g2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dhbw.eval1_g2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
