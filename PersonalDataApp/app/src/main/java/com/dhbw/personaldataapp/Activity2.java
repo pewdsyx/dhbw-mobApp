08b73dd43032058addae8ad1139355d481c5576f
Yves Staudenmaier <yves.staudenmaier@gmail.com>
2019-11-28 16:41:38 +0100

    added activity3

--- PersonalDataApp/app/src/main/java/com/dhbw/personaldataapp/Activity2.java
+++ PersonalDataApp/app/src/main/java/com/dhbw/personaldataapp/Activity2.java
@@ -3,6 +3,7 @@ package com.dhbw.personaldataapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+    }
+
+    public void toDialog3(View aView){
+
     }
 }
