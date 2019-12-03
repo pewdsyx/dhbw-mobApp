fefbf5c8615eab16f0d090bfea72040983e64a43
Yves Staudenmaier <yves.staudenmaier@gmail.com>
2019-12-03 16:27:56 +0100

    added EditText for Light Threshold input

--- berraschungsApp/app/src/main/java/com/dhbw/berraschungsapp/MainActivity.java
+++ berraschungsApp/app/src/main/java/com/dhbw/berraschungsapp/MainActivity.java
@@ -11,20 +11,23 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener, Runnable {
 
     private TextView surprise;
+    private EditText lightThresholdText;
     private SensorManager sensorManager;
     private Sensor lightSensor;
+    private double lightThreshold = 125;
 
-    private final double LIGHT_BORDER = 125.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.lightThresholdText = findViewById(R.id.lightThreshold);
         this.surprise = findViewById(R.id.surprise);
         this.surprise.setText("");
 
@@ -37,6 +40,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onClickStart(View aView) {
         Button myButton = (Button) aView;
         myButton.setText("schnell, schnell");
+
+        // editable Threshold with a EditText
+        if (this.lightThreshold != 125) {
+            this.lightThreshold = Double.parseDouble(this.lightThresholdText.getText().toString());
+        }
+
         Handler handler = new Handler();
         handler.postDelayed(this, 5000);
     }
@@ -46,7 +55,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onSensorChanged(SensorEvent event) {
         // change only if there is light present
         double lightValue = event.values[0];
-        if (lightValue >= LIGHT_BORDER) {
+
+        if (lightValue >= this.lightThreshold) {
             this.surprise.setText("Surprise!");
             this.sensorManager.unregisterListener(this,this.lightSensor);
         }
