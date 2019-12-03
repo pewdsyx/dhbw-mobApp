735820847b7cf40d1e6513a9691de126d52b0dfa
Yves Staudenmaier <yves.staudenmaier@gmail.com>
2019-12-03 14:46:16 +0100

    initial SensorList

--- sensors/app/src/main/java/com/dhbw/sensors/MainActivity.java
+++ sensors/app/src/main/java/com/dhbw/sensors/MainActivity.java
@@ -2,7 +2,12 @@ package com.dhbw.sensors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        for (Sensor s : sensorList) {
+            System.out.println(s.getName());
+        }
     }
 }
