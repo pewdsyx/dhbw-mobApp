098d4e5b7c36590fea9bfcaa3f0f04b0ecff6b93
Yves Staudenmaier <yves.staudenmaier@gmail.com>
2019-12-10 15:45:50 +0100

    initial commit

---
+++ eval2/app/src/main/java/com/example/eval2/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.eval2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager sensorManager;
+    Sensor whatEverFuckHeWants;
+
+    // data:
+    ArrayList<Double> refList;
+    ArrayList<Double> testList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        this.whatEverFuckHeWants = this.sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
+
+    }
+
+    // button handler:
+    public void onClickStart(View aView) {
+        // aktivate sensor:
+        // this.sensorManager.registerListener(this, this.whatEverFuckHeWants, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // data from sensor
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // dont need that method.
+    }
+}
