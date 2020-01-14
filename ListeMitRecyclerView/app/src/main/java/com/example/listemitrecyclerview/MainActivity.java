120b97e9277d9fa4da174079c6180e9174a94aa8
Yves Staudenmaier <s171988@student.dhbw-mannheim.de>
2020-01-14 17:11:37 +0100

    some stuff

--- ListeMitRecyclerView/app/src/main/java/com/example/listemitrecyclerview/MainActivity.java
+++ ListeMitRecyclerView/app/src/main/java/com/example/listemitrecyclerview/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> mData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mData = new ArrayList<>();
+
+        mData.add("1");
+        mData.add("1");
+        mData.add("1");
+
+        MyAdapter myAdapter = new MyAdapter(mData);
     }
 }
---
+++ ListeMitRecyclerView/app/src/main/java/com/example/listemitrecyclerview/MyAdapter.java
@@ -0,0 +1,34 @@
+package com.example.listemitrecyclerview;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter {
+
+    ArrayList<String> mData;
+
+    public MyAdapter(ArrayList<String> aData){
+        this.mData = aData;
+    }
+
+    
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
