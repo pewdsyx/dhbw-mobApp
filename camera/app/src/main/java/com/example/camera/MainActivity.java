5d5850c24fe6fbd1d445e77f0e1fe4d13dca9c62
Yves Staudenmaier <s171988@student.dhbw-mannheim.de>
2020-01-28 16:19:56 +0100

    save image at private space

--- camera/app/src/main/java/com/example/camera/MainActivity.java
+++ camera/app/src/main/java/com/example/camera/MainActivity.java
@@ -1,18 +1,51 @@
 package com.example.camera;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // open System Camera
         Intent startCameraApp = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES); // private area
+
+        try {
+            File imageFile = File.createTempFile("testImage", ".jpg", dir);
+            Uri uri = FileProvider.getUriForFile(this, "com.example.camera.fileprovider", imageFile);
+            startCameraApp.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+            startActivityForResult(startCameraApp, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        System.out.println("app returns");
+        ImageView imageView = findViewById(R.id.imageView);
+        /*Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);*/
+
     }
 }
